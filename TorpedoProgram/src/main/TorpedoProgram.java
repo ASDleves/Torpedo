371f8f80d3392731c8263c4bdc5d96c55841c0da
Szazidayum <szabrinamolnar@gmail.com>
2022-05-16 15:40:44 +0200

    hajó generálás

--- TorpedoProgram/src/main/TorpedoProgram.java
+++ TorpedoProgram/src/main/TorpedoProgram.java
@@ -1,16 +1,28 @@
 package main;
 
+import java.util.Random;
+
 public class TorpedoProgram {
 static String[] palya = new String[7];
     public static void main(String[] args) {
         PalyaGeneral();
-       
+        HajoGeneral();
     }
     public static void PalyaGeneral(){
      for (int i = 0; i < palya.length ; i++) {
             palya[i]="_";
             System.out.print(palya[i]+" ");
+        } 
+    }
+    public static void HajoGeneral(){
+        Random rnd = new Random();
+        int rHely = rnd.nextInt(7);
+            for (int j = 0; j < palya.length; j++) {
+                palya[rHely]="X";
         }
-     
+        
+        for (int i = 0; i < palya.length; i++) {
+            System.out.println(palya[i]);
+        }
     }
 }
