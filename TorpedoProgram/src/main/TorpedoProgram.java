ee0231147b620b4281922b36bac1d49f2c43e97e
Szazidayum <szabrinamolnar@gmail.com>
2022-05-16 16:00:04 +0200

    torpedo jatékos tipp

--- TorpedoProgram/src/main/TorpedoProgram.java
+++ TorpedoProgram/src/main/TorpedoProgram.java
@@ -1,12 +1,18 @@
 package main;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class TorpedoProgram {
 static String[] palya = new String[7];
+static String[] hajo = new String[3];
+static int rHely;
+
     public static void main(String[] args) {
         PalyaGeneral();
+        System.out.println(" ");
         HajoGeneral();
+        JatekosLoves(rHely);
     }
     public static void PalyaGeneral(){
      for (int i = 0; i < palya.length ; i++) {
@@ -16,13 +22,28 @@ static String[] palya = new String[7];
     }
     public static void HajoGeneral(){
         Random rnd = new Random();
-        int rHely = rnd.nextInt(7);
+        rHely = rnd.nextInt(5);
             for (int j = 0; j < palya.length; j++) {
                 palya[rHely]="X";
+                palya[rHely+1]="X";
+                palya[rHely+2]="X";
         }
         
         for (int i = 0; i < palya.length; i++) {
-            System.out.println(palya[i]);
+            System.out.print(palya[i]+" ");
+        }
+       
+    }
+    public static void JatekosLoves(int rHely){
+        System.out.println("\nKérlek add meg a tippelt számot! (1-7):");
+        Scanner sc = new Scanner(System.in);
+        int loves = sc.nextInt();
+        for (int i = 0; i < palya.length; i++) {
+            if (loves==rHely) {
+                System.out.println("Talált!");
+            }else{
+                System.out.println("Nem talált!");
+            }
         }
     }
 }
